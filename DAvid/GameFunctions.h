d717c1213262707d832afcdc9d6b2a9a0a3db9e1
Howard Perkins <hperkins@kishmish.com>
2025-07-16 18:02:38 -0400

    cleanup

--- DAvid/GameFunctions.h
+++ DAvid/GameFunctions.h
@@ -17,102 +17,69 @@ public:
 		return *(int*)value;
 	}
 
-	static int SendToClient(BYTE *packet, int Length)
+	static int SendToClient(BYTE *packet, int length)
 	{
+		if (length <= 0) return -1;
+		
+		LPVOID cave = VirtualAllocEx(GetCurrentProcess(), nullptr, length, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		if (!cave) return -1;
+		
+		memcpy(cave, packet, length);
 
-		if (Length > 0)
-		{
-
-			__asm
-			{
-				pushfd
-				pushad
-			}
-
-
-			int cave = (int)VirtualAllocEx(GetCurrentProcess(), NULL, Length, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-			memcpy((void*)cave, (void*)packet, Length);
-
-			int Recv = recvPacketin;
-			int PacketLength = Length;
-
-			__asm
-			{
-				pop ecx
-
-				mov edx, PacketLength
-				push edx
-
-				mov eax, cave
-				push eax
-
-				call Recv
-				lea ecx, [ebp - 00000405]
-				push ecx
-			}
-
-			__asm
-			{
-				popad
-				popfd
-			}
-
-			VirtualFreeEx(GetCurrentProcess(), (void*)cave, 0, MEM_RELEASE);
-
-			return 0;
-
-
-		}
-	}
-
-	static int SendToServer(BYTE *packet, int Length)
-	{
-		int cave = (int)VirtualAllocEx(GetCurrentProcess(), NULL, Length, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-		memcpy((void*)cave, (void*)packet, Length);
+		int Recv = recvPacketin;
+		int PacketLength = length;
 
 		__asm
 		{
 			pushfd
 			pushad
-		}
-
-
-		int SenderID = GetSenderID();
-		int Send = sendOffset;
-		int PacketLength = Length;
-
-		__asm
-		{
+			pop ecx
 			mov edx, PacketLength
 			push edx
-
 			mov eax, cave
 			push eax
-
-			mov ecx, [SenderID]
-			call Send
-		}
-
-		__asm
-		{
+			call Recv
+			lea ecx, [ebp - 00000405]
+			push ecx
 			popad
 			popfd
 		}
 
-		VirtualFreeEx(GetCurrentProcess(), (void*)cave, 0, MEM_RELEASE);
+		VirtualFreeEx(GetCurrentProcess(), cave, 0, MEM_RELEASE);
+
+		return 0;
+	}
+
+	static int SendToServer(BYTE *packet, int length)
+	{
+		if (length <= 0) return -1;
+		
+		LPVOID cave = VirtualAllocEx(GetCurrentProcess(), nullptr, length, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		if (!cave) return -1;
+		
+		memcpy(cave, packet, length);
+
+		int SenderID = GetSenderID();
+		int Send = sendOffset;
+		int PacketLength = length;
+		
+		__asm
+		{
+			pushfd
+			pushad
+			mov edx, PacketLength
+			push edx
+			mov eax, cave
+			push eax
+			mov ecx, [SenderID]
+			call Send
+			popad
+			popfd
+		}
+
+		VirtualFreeEx(GetCurrentProcess(), cave, 0, MEM_RELEASE);
 
 		return 0;
 	}
 
 };
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-#define DLL __declspec(dllexport)
-	typedef void(__stdcall * ProgressCallback)(int);
-	DLL void OnAction(ProgressCallback progressCallback);
-#ifdef __cplusplus
-}
-#endif
