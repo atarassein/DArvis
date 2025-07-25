bc3a93927a8ef1ac70619f535200da38ffe4a032
Howard Perkins <hperkins@kishmish.com>
2025-07-24 22:26:57 -0400

    SideQuest: toast notification agent

---
+++ SideQuest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SideQuest;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
